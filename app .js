7874fe501f2cc25e5d83e6824fde72db3ace486d
Juan Paulo Passi <passipaulo14@gmail.com>
2024-09-12 09:49:46 +0800

    Create app .js

---
+++ app .js
@@ -0,0 +1,21 @@
+// Create a variable name firstFavNum and secondFavNum store your favorite number
+
+let  = firstFavNum1 = 6;
+
+let = secondFavNum2 = 23;
+
+
+
+// Perform arithmetic operations 
+
+console.log("Addition:", firstFavNum1 + secondFavNum2);
+
+console.log("Subtraction:", firstFavNum1 - secondFavNum2);
+
+console.log("Multiplication:", firstFavNum1 * secondFavNum2);
+
+console.log("Division:", firstFavNum1 / secondFavNum2);
+
+console.log("Moduls:", firstFavNum1 % secondFavNum2);
+
+console.log("Exponentiation(Power):", firstFavNum1 ** secondFavNum2);
